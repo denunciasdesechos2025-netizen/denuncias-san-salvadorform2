87acc03ea255c9bfa411703691c2a183571bc9a0
denunciasdesechos2025-netizen <denunciasdesechos2025@gmail.com>
2026-01-16 11:49:50 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 const CONFIG = {
-    GEMINI_API_KEY: "",
+    GEMINI_API_KEY: "AIzaSyAi7hWSVsOZrUv7ZHdi53rwR8C8QkDiLek",
         GOOGLE_SCRIPT_URL: ""
 };
 
 // Exportar para que sea accesible globalmente si es necesario
 window.CONFIG = CONFIG;
+
 
 
